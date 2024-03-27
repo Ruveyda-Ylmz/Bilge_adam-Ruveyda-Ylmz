5a9d2cad8eea457c6df7e3b12955b2a5e60e2918
Ruveyda-Ylmz <ylmzruveyda44@gmail.com>
2024-03-27 17:06:29 +0300

    file io ogrenildi

---
+++ src/Week03/oop/animal.java
@@ -0,0 +1,5 @@
+package Week03.oop;
+
+public class animal {
+    String name;
+}
---
+++ src/Week03/oop/test.java
@@ -0,0 +1,33 @@
+package Week03.oop;
+
+public class test {
+    public static void main(String[] args) {
+        ClassName className = new ClassName();
+        System.out.println(className.sayhello("ruveyda"));
+        //className.sayhello("ruveya");
+        System.out.println(className.sum(1,5));
+
+        ClassName className2 = new ClassName();
+        System.out.println(className.sayhello("muuyyzdiiif"));
+        System.out.println(className2.sum(8,12));
+
+
+        //person
+
+        Person p1 = new Person(); //nesne
+        p1.name = "ruveyda";
+        p1.nickname= "rowi";
+        p1.gender ="male";
+
+        p1.sleep();
+        p1.walk();
+
+        Person p2 = new Person();
+        p2.name ="gamze";
+        p2.nickname ="gazo";
+        p2.sleep();
+        p2.walk();
+
+        System.out.println(p2.nickname);
+    }
+}
