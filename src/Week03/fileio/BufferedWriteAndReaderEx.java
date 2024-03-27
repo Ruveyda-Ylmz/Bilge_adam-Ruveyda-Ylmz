8c874656220c31f5c9c670f04ec63dbb032fc1b5
Ruveyda-Ylmz <ylmzruveyda44@gmail.com>
2024-03-27 12:53:58 +0300

    file io ogrenildi

---
+++ src/Week03/fileio/BufferedWriteAndReaderEx.java
@@ -0,0 +1,35 @@
+package Week03.fileio;
+
+import hafta02.WhileLoop;
+
+import java.io.*;
+
+public class BufferedWriteAndReaderEx {
+    public BufferedWriteAndReaderEx() throws FileNotFoundException {
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("buffered.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write("hello");
+        bw.write("word");
+        bw.newLine();
+        bw.write("java");
+        bw.close();
+        fw.close();
+
+
+    FileReader fr = new FileReader("buffered.txt");
+    BufferedReader br = new BufferedReader(fr);
+
+    String data = null; //obje oluşturuduğumuzdan null değeri olmayackatır bu sebepten null ile karşılaştırıyoruz
+
+    while((data = br.readLine()) != null){ //satırı okur
+        System.out.println(data);
+
+    }
+    fr.close();
+    br.close();
+
+    }
+}
---
+++ src/Week03/fileio/PrintwriteExample.java
@@ -0,0 +1,16 @@
+package Week03.fileio;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class PrintwriteExample {
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter pw = new PrintWriter("printWrite.txt");
+        pw.println("hello");
+        pw.println("word");
+        pw.println();
+        pw.println("java");
+        pw.flush();
+        pw.close();
+    }
+}
