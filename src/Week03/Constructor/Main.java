365d75f2c2c12443e5874ef10265a2e95d5ff087
Ruveyda-Ylmz <ylmzruveyda44@gmail.com>
2024-03-29 09:46:11 +0300

    constructor ve garbagecCollector oğrenildi

---
+++ src/Week03/Constructor/Main.java
@@ -0,0 +1,16 @@
+package Week03.Constructor;
+
+public class Main {
+    private String name;
+    Main(){
+        System.out.println("Constructor Called...");
+        name = "Constructor name field.";
+    }
+    public static void main(String[] args) {
+        Main obj = new Main();
+        Main obj2 = new Main();
+//        obj.name ="Constructor name field.";
+//        obj2.name="Constructor name field.";
+        System.out.println("The name is : " + obj.name+"\nThe second name is : " + obj2.name);
+    }
+}
---
+++ src/Week03/garbagecollector/Animal.java
@@ -0,0 +1,12 @@
+package Week03.garbagecollector;
+
+public class Animal {
+    String name;
+
+    Animal() {
+    } //empty/parametresiz constructor. NoArgConstructor
+
+    Animal(String name) { //parametreli constructor metodu.
+        this.name = name;
+    }
+}
