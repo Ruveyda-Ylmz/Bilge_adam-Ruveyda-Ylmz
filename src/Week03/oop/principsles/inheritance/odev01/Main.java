4a5c513e0dee5e7d695f38e8d2de504c2ff8d717
Ruveyda-Ylmz <ylmzruveyda44@gmail.com>
2024-04-01 20:55:22 +0300

    yeni

--- src/Week03/oop/principsles/inheritance/odev01/Main.java
+++ src/Week03/oop/principsles/inheritance/odev01/Main.java
@@ -8,41 +8,41 @@ public class Main {
         Motorcycle motorcycle= new Motorcycle();
 
         truck.cargoCapacity=4;
-        truck.year=2023;
+        truck.year=2000;
         truck.model="xx";
-        truck.brand="BMW";
+        truck.brand="Volvo";
 
         car.numDoors=4;
         car.year=2000;
         car.model="xx";
-        car.brand="xx";
+        car.brand="DS";
 
         motorcycle.hasSidecar=false;
         motorcycle.year=2000;
         motorcycle.model="xx";
-        motorcycle.brand="xx";
+        motorcycle.brand="Yamaha";
 
         truck.start();
-        System.out.println("-TRUCK-"+"\n"+"cargoCapacity: "+truck.cargoCapacity+"\n"+
+        System.out.println("-TRUCK-"+"\n"+"cargoCapacity= "+truck.cargoCapacity+"\n"+
                                       "year= "+truck.year+"\n"+
                                       "model= "+truck.model+"\n"+
-                                      "brand"+truck.brand);
+                                      "brand= "+truck.brand);
         truck.stop();
         System.out.println("---------------------------");
 
         car.start();
-        System.out.println("-CAR-"+"\n"+"numDoors: "+car.numDoors+"\n"+
+        System.out.println("-CAR-"+"\n"+"numDoors= "+car.numDoors+"\n"+
                                       "year= "+car.year+"\n"+
                                       "model= "+car.model+"\n"+
-                                      "brand"+car.brand);
+                                      "brand= "+car.brand);
         car.stop();
         System.out.println("----------------------------");
 
         motorcycle.start();
-        System.out.println("-MOTORCYCLE-"+"\n"+"hasSidecar"+motorcycle.hasSidecar+"\n"+
+        System.out.println("-MOTORCYCLE-"+"\n"+"hasSidecar= "+motorcycle.hasSidecar+"\n"+
                                                 "year= "+motorcycle.year+"\n"+
                                                 "model= "+motorcycle.model+"\n"+
-                                                "brand"+motorcycle.brand);
+                                                "brand= "+motorcycle.brand);
 
         motorcycle.stop();
         System.out.println("----------------------------");
